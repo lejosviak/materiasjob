820cc94ab1ee05c1dc11652535330fc2d6bff3df
lejosviak <163347879+lejosviak@users.noreply.github.com>
2025-03-28 21:50:40 -0300

    dh

--- scripts.js
+++
@@ -1,8 +0,0 @@
-
-function showContent(discipline) {
-    let contents = document.querySelectorAll('.discipline-content');
-    contents.forEach(content => content.classList.remove('active'));
-
-    let selectedContent = document.getElementById(discipline);
-    selectedContent.classList.add('active');
-}
